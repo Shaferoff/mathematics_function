66b6a9468fbd109fd8fc0898ef385863a24e3b6e
Shaferoff <shaferoff11@gmail.com>
2024-08-02 20:15:24 +0300

    mathematics_function

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Math.abs(-3));
+        System.out.println(Math.pow(3,2));
+        System.out.println(Math.sqrt(4));
+        System.out.println(Math.round(0.9));
+        System.out.println(Math.floor(-9.8));
+        System.out.println(Math.ceil(-9.8));
+        System.out.println(Math.min(-9,8));
+        System.out.println(Math.max(-9,8));
+        System.out.println(Math.random());
+    }
+}
